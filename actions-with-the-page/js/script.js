207cd61e0ca945b51d999f16202c1798d2250f2b
Kuleshov Artem <kooleshov.artem@gmail.com>
2022-10-13 16:30:03 +0500

    работа с элементами на странице. удаление и добавление элементов

---
+++ actions-with-the-page/js/script.js
@@ -0,0 +1,53 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
+'use strict';
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
+const adv = document.querySelectorAll('.promo__adv img');
+const genre = document.querySelector('.promo__genre');
+const promoBg = document.querySelector('.promo__bg');
+const interactiveList = document.querySelector('.promo__interactive-list');
+
+adv.forEach(item => 
+    item.remove());
+
+genre.textContent = 'драма';
+
+promoBg.style.backgroundImage = 'url("../img/bg.jpg")';
+
+interactiveList.innerHTML = '';
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((item, index) => {
+    interactiveList.innerHTML += `
+    <li class="promo__interactive-item">${index+1} ${item}
+         <div class="delete"></div>
+    </li>`;
+});
+
+
+
+
+
