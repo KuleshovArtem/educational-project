2cd4ba0b1f2d0c7ee8489d8c2b07a2442e25b365
Kuleshov Artem <kooleshov.artem@gmail.com>
2022-10-26 23:10:10 +0500

    фиксация изменений

---
+++ codewars/script.js
@@ -0,0 +1,24 @@
+// var min = function(list){
+//     list.sort(compareNum);
+
+//     return list[0];
+// }
+
+// var max = function(list){
+//     list.sort(compareNum).reverse();
+
+//     return list[0];
+// }
+
+// function compareNum(a, b) {
+//     return a - b;
+// }
+
+// console.log(min([4,6,2,1,9,63,-134,566]));
+
+// function correct(string)
+// {
+//     return string.replace(/'5'/g,'S').replace(/'0'/g,'O').replace(/'1'/g,'I');
+    
+// }
+// console.log(correct('fg05dil'));
