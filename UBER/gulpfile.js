ad6ee686cbffef4ea3e882f8b2cd5448dde9d38b
artem kuleshov <kooleshov.artem@gmail.com>
2022-11-11 20:10:14 +0500

    настройка sass и gulp

--- UBER/gulpfile.js
+++ UBER/gulpfile.js
@@ -1,6 +1,9 @@
 const gulp        = require('gulp');
 const browserSync = require('browser-sync');
 const sass = require('gulp-sass')(require('sass'));
+const rename = require("gulp-rename");
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
 
 // Static server
 gulp.task('server', function() {
@@ -11,9 +14,17 @@ gulp.task('server', function() {
     });
 });
 
-gulp.task('style', function () {
+gulp.task('styles', function () {
     return gulp.src("src/sass/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({
+            prefix: "",
+            suffix: ".min",
+          }))
+        .pipe(autoprefixer({
+			cascade: false
+		}))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest('srs/css'))
         .pipe(browserSync.stream());
 });
