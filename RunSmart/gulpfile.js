b838b9cadd1d1559356238d84ea6239a65bcf7d9
artem kuleshov <kooleshov.artem@gmail.com>
2022-11-12 15:20:07 +0500

    настройка gulp, частичная верстка первой секции по БЭМ

--- RunSmart/gulpfile.js
+++ RunSmart/gulpfile.js
@@ -15,7 +15,7 @@ gulp.task('server', function() {
 });
 
 gulp.task('styles', function () {
-    return gulp.src("src/sass/*.+(scss|sass)")
+    return gulp.src("src/sass/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({
             prefix: "",
@@ -30,7 +30,7 @@ gulp.task('styles', function () {
 });
 
 gulp.task('watch', function () {
-    gulp.watch('src/sass/*.+(scss|sass)', gulp.parallel('styles'));
+    gulp.watch('src/sass/**/*.+(scss|sass)', gulp.parallel('styles'));
     gulp.watch('src/*.html').on('change', browserSync.reload);
 
 });
