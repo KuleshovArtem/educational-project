34e46da10ee2166b2bbd63ae024b0ae9bed9b2f8
artem kuleshov <kooleshov.artem@gmail.com>
2022-11-16 19:06:08 +0500

    переделка carousel, отказ от jquery

--- RunSmart/src/js/script.js
+++ RunSmart/src/js/script.js
@@ -1,37 +1,25 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1200,
-        // adaptiveHeight: true,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        prevArrow: '<button type="button" class="slick-prev"> <img src="icons/arrow-left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"> <img src="icons/arrow-right.svg"></button>',
-        responsive: [
-            {
-            breakpoint: 992,
-            settings: {
-                dots: true,
-                arrows: false,
-            }
-            },
-            // {
-            // breakpoint: 600,
-            // settings: {
-            //     slidesToShow: 2,
-            //     slidesToScroll: 2
-            // }
-            // },
-            // {
-            // breakpoint: 480,
-            // settings: {
-            //     slidesToShow: 1,
-            //     slidesToScroll: 1
-            // }
-            // }
-            // // You can unslick at a given breakpoint now by adding:
-            // // settings: "unslick"
-            // // instead of a settings object
-        ]
-      });
+ const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    autoplayButtonOutput: false,
+    nav: false,
+    controls: false,
+    // responsive: {
+    //     992: {
+    //       edgePadding: 20,
+    //       gutter: 20,
+    //       items: 1
+    //     },
+    //   }
   });
-      
+
+  document.querySelector('.next').addEventListener('click', function () {
+        slider.goTo('next');
+    });
+
+    document.querySelector('.prev').addEventListener('click', function () {
+        slider.goTo('prev');
+    });
+    
