8ce625a4fdb7596e58f816c19247f6500387ebc2
artem kuleshov <kooleshov.artem@gmail.com>
2022-11-28 14:05:48 +0500

    реализация работы табов на JS с jquery

--- RunSmart/src/js/script.js
+++ RunSmart/src/js/script.js
@@ -6,20 +6,36 @@
     autoplayButtonOutput: false,
     nav: false,
     controls: false,
-    // responsive: {
-    //     992: {
-    //       edgePadding: 20,
-    //       gutter: 20,
-    //       items: 1
-    //     },
-    //   }
-  });
+});
 
-  document.querySelector('.next').addEventListener('click', function () {
-        slider.goTo('next');
-    });
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+});
 
-    document.querySelector('.prev').addEventListener('click', function () {
-        slider.goTo('prev');
-    });
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
     
+(function($) {
+$(function() {
+    
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide (item) {
+        $(item).each(function(i) {
+            $(this).on('click', function (event) {
+                event.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            });
+        });
+    }
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+    
+});
+})(jQuery);   
