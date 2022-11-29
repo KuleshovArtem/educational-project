8408be3093f6730f22f1f02afa03ebd062b90972
artem kuleshov <kooleshov.artem@gmail.com>
2022-11-29 11:02:05 +0500

    реализация валидации форм с помощью jquery

--- RunSmart/src/js/script.js
+++ RunSmart/src/js/script.js
@@ -52,7 +52,39 @@ $(function() {
             $('.overlay, #order').fadeIn();
         });
     });
-    
+
+    function validateForms (form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                  },   
+                phone: 'required',
+                email: {
+                    required: true,
+                    email: true,
+                },
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Ввидите минимум {0} символа!")
+                } ,
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Неправильно введен адрес почты. Правильный формат name@domain.ru"
+                }
+              }
+        });
+    }
+    validateForms('#order form');
+    validateForms('#consultation form');
+    validateForms('#consultation-form');
+  
+
+   
     
 });
 })(jQuery);   
