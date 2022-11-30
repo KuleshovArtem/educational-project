488cc0573e203b219f285d23db05035c7cb071c3
artem kuleshov <kooleshov.artem@gmail.com>
2022-11-30 17:41:41 +0500

    проведена валидация файла html

--- RunSmart/src/js/script.js
+++ RunSmart/src/js/script.js
@@ -121,14 +121,14 @@ $(function() {
           event.preventDefault();
 
           const hash = this.hash;
-          
+
           $('html, body').animate({
             scrollTop: $(hash).offset().top
           }, 1000, function(){
             window.location.hash = hash;
           });
         }
-      });
+    });
    
     
 });
