024e5171eb123842af4598d58fe29d5a59e95eb8
artem kuleshov <kooleshov.artem@gmail.com>
2022-12-10 11:00:18 +0500

    фиксация изменений

---
+++ RunSmart/dist/js/script.js
@@ -0,0 +1,137 @@
+ const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    autoplayButtonOutput: false,
+    nav: false,
+    controls: false,
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+    
+(function($) {
+$(function() {
+    
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide (item) {
+        $(item).each(function(i) {
+            $(this).on('click', function (event) {
+                event.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            });
+        });
+    }
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+
+    // modal
+    $('[data-modal=consultation]').on('click', function () {
+        $('.overlay, #consultation').fadeIn();
+    });
+
+    $('.modal__close').on('click', function () {
+        $('.overlay, #consultation, #order, #thanks').fadeOut();
+    });
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function () {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn();
+        });
+    });
+
+    function validateForms (form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                  },   
+                phone: 'required',
+                email: {
+                    required: true,
+                    email: true,
+                },
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Ввидите минимум {0} символа!")
+                } ,
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Неправильно введен адрес почты. Правильный формат name@domain.ru"
+                }
+              }
+        });
+    }
+    validateForms('#order form');
+    validateForms('#consultation form');
+    validateForms('#consultation-form');
+    
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+
+    $('form').submit(function (e) {
+        e.preventDefault();
+
+        if(!$(this).valid()) {
+            return;
+        }
+        $.ajax({
+            type:'POST',
+            url: 'mailer/smart.php',
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find('input').val('');
+            $('#order, #consultation').fadeOut();
+            $('.overlay, #thanks').fadeIn();
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a").on('click', function(event) {
+
+        if (this.hash !== "") {
+          event.preventDefault();
+
+          const hash = this.hash;
+
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 1000, function(){
+            window.location.hash = hash;
+          });
+        }
+    });
+   
+    
+});
+})(jQuery);  
+
+new WOW().init();
--- RunSmart/gulpfile.js
+++ RunSmart/gulpfile.js
@@ -4,14 +4,18 @@ const sass = require('gulp-sass')(require('sass'));
 const rename = require("gulp-rename");
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
 
 // Static server
 gulp.task('server', function() {
     browserSync.init({
         server: {
-            baseDir: "src"
+            baseDir: "dist"  // папка запуска сервера src or dist
         }
     });
+
+    gulp.watch('src/*.html').on('change', browserSync.reload);  // в уроке появилась тут
 });
 
 gulp.task('styles', function () {
@@ -25,14 +29,52 @@ gulp.task('styles', function () {
 			cascade: false
 		}))
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest('src/css'))
+        .pipe(gulp.dest('dist/css'))  // папка компиляции css файла src/css or dist/css
         .pipe(browserSync.stream());
 });
-
+//компиляция scss/sass файлов
 gulp.task('watch', function () {
-    gulp.watch('src/sass/**/*.+(scss|sass)', gulp.parallel('styles'));
-    gulp.watch('src/*.html').on('change', browserSync.reload);
+    gulp.watch('src/sass/**/*.+(scss|sass|css)', gulp.parallel('styles'));
+    // gulp.watch('src/*.html').on('change', browserSync.reload);   //в уроке его не было
+    gulp.watch('src/*.html').on('change', gulp.parallel('html')); //добавил новые описание
 
 });
+// сжатие и компирование html в dist
+gulp.task('html', function () {
+    return gulp.src("src/*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('dist/'));
+});
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+// копирование js файлов в dist
+gulp.task('scripts', function () {
+    return gulp.src("src/js/**/*.js")
+        .pipe(gulp.dest('dist/js'));
+});
+
+//копирование fonts в dist
+gulp.task('fonts', function () {
+    return gulp.src("src/fonts/**/*")
+        .pipe(gulp.dest('dist/fonts'));
+});
+
+//копирование icons в dist
+gulp.task('icons', function () {
+    return gulp.src("src/icons/**/*")
+        .pipe(gulp.dest('dist/icons'));
+});
+
+//копирование mailer в dist
+gulp.task('mailer', function () {
+    return gulp.src("src/mailer/**/*")
+        .pipe(gulp.dest('dist/mailer'));
+});
+
+//копирование img в dist
+gulp.task('img', function () {
+    return gulp.src("src/img/**/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'html', 'scripts', 'fonts', 'icons', 'mailer', 'img' ));
